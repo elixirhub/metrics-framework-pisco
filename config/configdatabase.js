29a1b33858cbaa8c2a41a63c0bc9448856b0ed38
haydeeartaza <haydee.artaza@tgac.ac.uk>
2016-01-21 14:26:42 +0000

    New structure

--- config/configdatabase.js
+++
@@ -1,6 +0,0 @@
-
-/**
-Acces to database
-**/	
-
-module.exports.URL = 'mongodb://localhost:27017/metricsdb';
--- installcomponents.js
+++
@@ -1,3 +0,0 @@
-var MC = require('./lib/managementcomponents.js');
-var managementcomponents = new MC();
-managementcomponents.installComponents();
